dad16c87db68f046ced23bd4e57cc8b93aa2c1cb
aekostadinov <aemilov01@gmail.com>
2024-09-20 23:57:26 +0300

    Adding my first test

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -141,4 +141,8 @@ contract Raffle is VRFConsumerBaseV2Plus {
     function getEntranceFee() external view returns (uint) {
         return i_entranceFee;
     }
+
+    function getRaffleState() external view returns (RaffleState) {
+        return s_raffleState;
+    }
 }
